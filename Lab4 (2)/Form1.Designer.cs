e42fac58f19ccc7e022b2f2a7faafbf4b8dd4f9e
rrmahb01 <31289795+rrmahb01@users.noreply.github.com>
2017-11-08 11:31:16 -0500

    Add files via upload

---
+++ Lab4 (2)/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Lab4__2_
+{
+    partial class acceptOrReject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.decideButton = new System.Windows.Forms.Button();
+            this.acceptTotalLabel = new System.Windows.Forms.Label();
+            this.gpaTextBox = new System.Windows.Forms.TextBox();
+            this.rejectTotalLabel = new System.Windows.Forms.Label();
+            this.testTextBox = new System.Windows.Forms.TextBox();
+            this.gpaLabel = new System.Windows.Forms.Label();
+            this.testLabel = new System.Windows.Forms.Label();
+            this.acceptLabel = new System.Windows.Forms.Label();
+            this.rejectLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // decideButton
+            // 
+            this.decideButton.Location = new System.Drawing.Point(164, 129);
+            this.decideButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.decideButton.Name = "decideButton";
+            this.decideButton.Size = new System.Drawing.Size(112, 35);
+            this.decideButton.TabIndex = 0;
+            this.decideButton.Text = "Decide";
+            this.decideButton.UseVisualStyleBackColor = true;
+            this.decideButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // acceptTotalLabel
+            // 
+            this.acceptTotalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.acceptTotalLabel.Location = new System.Drawing.Point(144, 203);
+            this.acceptTotalLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.acceptTotalLabel.Name = "acceptTotalLabel";
+            this.acceptTotalLabel.Size = new System.Drawing.Size(149, 34);
+            this.acceptTotalLabel.TabIndex = 1;
+            this.acceptTotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gpaTextBox
+            // 
+            this.gpaTextBox.Location = new System.Drawing.Point(254, 18);
+            this.gpaTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gpaTextBox.Name = "gpaTextBox";
+            this.gpaTextBox.Size = new System.Drawing.Size(148, 26);
+            this.gpaTextBox.TabIndex = 2;
+            this.gpaTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rejectTotalLabel
+            // 
+            this.rejectTotalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rejectTotalLabel.Location = new System.Drawing.Point(144, 255);
+            this.rejectTotalLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.rejectTotalLabel.Name = "rejectTotalLabel";
+            this.rejectTotalLabel.Size = new System.Drawing.Size(149, 34);
+            this.rejectTotalLabel.TabIndex = 3;
+            this.rejectTotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // testTextBox
+            // 
+            this.testTextBox.Location = new System.Drawing.Point(254, 58);
+            this.testTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.testTextBox.Name = "testTextBox";
+            this.testTextBox.Size = new System.Drawing.Size(148, 26);
+            this.testTextBox.TabIndex = 4;
+            this.testTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // gpaLabel
+            // 
+            this.gpaLabel.AutoSize = true;
+            this.gpaLabel.Location = new System.Drawing.Point(54, 23);
+            this.gpaLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.gpaLabel.Name = "gpaLabel";
+            this.gpaLabel.Size = new System.Drawing.Size(188, 20);
+            this.gpaLabel.TabIndex = 5;
+            this.gpaLabel.Text = "Enter your GPA ( i.e. 3.0 )";
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(18, 63);
+            this.testLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(223, 20);
+            this.testLabel.TabIndex = 6;
+            this.testLabel.Text = "Enter your Test Score ( i.e 70 )";
+            // 
+            // acceptLabel
+            // 
+            this.acceptLabel.AutoSize = true;
+            this.acceptLabel.Location = new System.Drawing.Point(54, 218);
+            this.acceptLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.acceptLabel.Name = "acceptLabel";
+            this.acceptLabel.Size = new System.Drawing.Size(59, 20);
+            this.acceptLabel.TabIndex = 7;
+            this.acceptLabel.Text = "Accept";
+            // 
+            // rejectLabel
+            // 
+            this.rejectLabel.AutoSize = true;
+            this.rejectLabel.Location = new System.Drawing.Point(58, 271);
+            this.rejectLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.rejectLabel.Name = "rejectLabel";
+            this.rejectLabel.Size = new System.Drawing.Size(55, 20);
+            this.rejectLabel.TabIndex = 8;
+            this.rejectLabel.Text = "Reject";
+            // 
+            // acceptOrReject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 322);
+            this.Controls.Add(this.rejectLabel);
+            this.Controls.Add(this.acceptLabel);
+            this.Controls.Add(this.testLabel);
+            this.Controls.Add(this.gpaLabel);
+            this.Controls.Add(this.testTextBox);
+            this.Controls.Add(this.rejectTotalLabel);
+            this.Controls.Add(this.gpaTextBox);
+            this.Controls.Add(this.acceptTotalLabel);
+            this.Controls.Add(this.decideButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "acceptOrReject";
+            this.Text = "Accept or Reject";
+            this.Load += new System.EventHandler(this.acceptOrReject_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button decideButton;
+        private System.Windows.Forms.Label acceptTotalLabel;
+        private System.Windows.Forms.TextBox gpaTextBox;
+        private System.Windows.Forms.Label rejectTotalLabel;
+        private System.Windows.Forms.TextBox testTextBox;
+        private System.Windows.Forms.Label gpaLabel;
+        private System.Windows.Forms.Label testLabel;
+        private System.Windows.Forms.Label acceptLabel;
+        private System.Windows.Forms.Label rejectLabel;
+    }
+}
+
---
+++ Lab4 (2)/Form1.cs
@@ -0,0 +1,67 @@
+﻿//Lab 4
+//CIS 199-01
+//Due 2/16/2016
+//By: Rakesh R. Mahbubani
+
+//This program uses IF statements to compare the user's GPA and test score in order to decide if they are accepted or rejected.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4__2_
+{
+    public partial class acceptOrReject : Form
+    {
+        //declare double variables to store the information the user inputs
+        double GPA;
+        double Test;
+
+        //declare private int variables for the counters
+        private int acceptTotal = 0;
+        private int rejectTotal = 0;
+
+        public acceptOrReject()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //connects the double variables to their respective text boxes
+            GPA = double.Parse(gpaTextBox.Text);
+            Test = double.Parse(testTextBox.Text);
+
+            if (GPA >= 3.0 && Test >= 60)
+            {
+                MessageBox.Show("Accept"); //shows the message "Accept"
+                acceptTotal += 1; //increases the accept total by 1
+                acceptTotalLabel.Text = acceptTotal.ToString(); //displays the new accept total in the label
+            }
+            else if (GPA < 3.0 && Test >= 80)
+            {
+                MessageBox.Show("Accept"); //shows the message "Accept"
+                acceptTotal += 1; //increases the accept total by 1
+                acceptTotalLabel.Text = acceptTotal.ToString(); //displays the new accept total in the label
+            }
+            else
+            { 
+                MessageBox.Show("Reject"); //shows the message "Reject"
+                rejectTotal += 1; //increases the reject total by 1
+                rejectTotalLabel.Text = rejectTotal.ToString(); //displays the new reject total in the label
+            }
+
+        }
+
+        private void acceptOrReject_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
